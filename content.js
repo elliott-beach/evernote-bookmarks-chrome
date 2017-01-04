44b66570f4523b68fb909d83aa81234271801c28
Elliott Beach <elliott2.71828@gmail.com>
2017-01-04 13:57:24 -0600

    do things

--- content.js
+++ content.js
@@ -1,9 +1,20 @@
+console.log('loaded content script');
 
-function createNote(title, content){
-	$.get(`http://localhost:3000/create?title=${title}&content=${content}`);
+// creds to Martin Spier, https://github.com/spiermar/bookmarks2evernote/blob/5b357f7b0efd8e2002b8a000cc06ffd39a09a854/bm2evernote.py#L15-L17
+class Bookmark {
+	constructor(title, url){
+		this.title = title;
+		this.url = url;
+		// this.tag = tag;
+		this.content = `<a href="${url}">${title}</a>`
+	}
+
+	// @TODO change to post
+	create(){
+		$.get(`http://localhost:3000/create?title=${this.title}&content=${this.content}`);
+	}
 }
 
-console.log('loaded content script');
 
 const port = chrome.runtime.connect();
 
@@ -20,17 +31,18 @@ function process(bookmarks){
 	bookmarks.forEach(traverse);
 }
 
-function isFolder(treeNode){}
-
 // @TODO add ability to add tags based on bookmark tree.
+STOP = false
 function traverse(treeNode){
+	if(STOP) return;
 
 	if ( treeNode.children) {
 		treeNode.children.forEach(traverse);
 	} else {
 		// @TODO get title of web page (description?)
-		// createNote("bookmark", treeNode.url);
+		new Bookmark(treeNode.title, treeNode.url).create();
 		console.log(treeNode.url);
+		STOP = true;
 	}
 
 }
