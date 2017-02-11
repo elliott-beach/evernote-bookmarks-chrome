548908097e63889364f42c724a8eb37024c9ff2b
Elliott Beach <elliott2.71828@gmail.com>
2017-02-10 20:37:05 -0600

    send data in json, improve style

--- content.js
+++ content.js
@@ -1,9 +1,7 @@
 console.log('loaded content script');
 
-/* GLOBAL config */
-const HOSTNAME = config.HOSTNAME;
-
-//creds to hgoebl -- http://stackoverflow.com/questions/7918868/how-to-escape-xml-entities-in-javascript
+// Weird characters in the title/url can cause problems.
+// from http://stackoverflow.com/questions/7918868/how-to-escape-xml-entities-in-javascript
 function escapeXml(unsafe) {
     return unsafe.replace(/[<>&'"]/g, function (c) {
         switch (c) {
@@ -16,21 +14,38 @@ function escapeXml(unsafe) {
     });
 }
 
-// creds to Martin Spier -- https://github.com/spiermar/bookmarks2evernote/blob/5b357f7b0efd8e2002b8a000cc06ffd39a09a854/bm2evernote.py#L15-L17
 class Bookmark {
 	constructor(title, url){
 		this.title = escapeXml(title);
 		this.url = escapeXml(url);
-		// this.tag = tag;
 		this.content = `<a href="${this.url}">${this.title}</a>`
-		console.log(this.content);
+        this.jsonData = {
+            title: this.title,
+            content: this.content
+        }
 	}
 
-	create(){
-		$.post(HOSTNAME+'/create', {
-			title: this.title,
-			content: this.content
-		}).fail( (xhr, status, error) => {
+	static create(bookmarks){
+
+        // allow sending single bookmark
+        if (! Array.isArray(bookmarks) ){
+            bookmarks = [ bookmarks ];
+        }
+
+        // grab the info we care about
+        const data = bookmarks.map( (b) => b.jsonData );
+
+        // send teh post request
+		$.ajax({
+            type: 'post',
+            url: config.HOSTNAME + '/create',
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            success: function(data){
+                $(document.body).append('<p><em>Success!</em></p>')
+            }
+        })
+        .fail( (xhr, status, error) => {
 			console.log(xhr);
 			console.log(status);
 			console.log(error);
@@ -38,28 +53,38 @@ class Bookmark {
 	}
 }
 
-const port = chrome.runtime.connect();
 
+// wait for the background script to send us the bookmarks.
+const port = chrome.runtime.connect();
 port.onMessage.addListener( (message) => {
-	if (message.bookmarks){
-		process(message.bookmarks);
-		//test();
-	}
+    if (message.bookmarks){
+        process(message.bookmarks);
+    }
 });
+
 
 function process(bookmarks){
 	console.log(bookmarks);
-	bookmarks.forEach(traverse);
+    results = [];
+	bookmarks.forEach( b => traverse(b, results));
+    Bookmark.create(results);
 }
 
 // @TODO add ability to add tags based on bookmark tree.
-function traverse(treeNode){
+function traverse(treeNode, results){
 	if ( treeNode.children) {
 		treeNode.children.forEach(traverse);
 	} else {
-		new Bookmark(treeNode.title, treeNode.url).create();
+		let bookmark = new Bookmark(treeNode.title, treeNode.url);
+        results.push(bookmark);
 	}
 }
+
+
+/*************************
+BOOTSTRAP FUNCTIONS
+*************************/
+
 
 function main(){
 	port.postMessage('getBookmarks');
@@ -67,11 +92,11 @@ function main(){
 
 function test(){
 	// testing so many bookmarks is just painful, so I will use this.
-	new Bookmark('test', 'http://example.com').create();
+    bookmark = new Bookmark('test', 'http://example.com');
+	Bookmark.create([bookmark, bookmark]);
 }
 
-
-if (config.test){
+if (config.dryRun){
 	test();
 } else {
 	main();
